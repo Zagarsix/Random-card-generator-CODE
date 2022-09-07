014c399bcf5902d59d4845ac70b1e17fc517e23c
Zagarsix <claudk1006@gmail.com>
2022-09-07 03:48:40 -0400

    finished code

--- src/app.js
+++ src/app.js
@@ -1,72 +1,51 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import "../src/style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let cards = ["♠", "♣", "♥", "♦"];
 
-window.onload = function() {
-  function mazoDeCartas() {
-    const values = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-    const tipoCarta = ["Corazon", "Diamante", "Espada", "Trebol"];
-    const cartas = [];
-    for (let i = 0; i < tipoCarta.length; i++) {
-      for (let u = 0; u < values.length; u++) {
-        const pinta = tipoCarta[i];
-        const numero = values[u];
-        cartas.push({ numero, pinta });
-      }
-    }
-    return cartas;
-  }
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-  function cartaRandom(cartas) {
-    const random = Math.floor(Math.random() * 51);
-    const cartaNumero = cartas[random].numero;
-    const cartaPinta = cartas[random].pinta;
-    let icono;
+function randomCardIndex() {
+  return Math.floor(Math.random() * cards.length);
+}
 
-    cartaPinta === "Diamante"
-      ? (icono = `<i class="fa-solid fa-diamond"></i>`)
-      : cartaPinta === "Corazon"
-      ? (icono = `<i class="fa-solid fa-heart"></i>`)
-      : cartaPinta === "Espada"
-      ? (icono = `&spades;`)
-      : cartaPinta === "Trebol"
-      ? (icono = ` &clubs;`)
-      : (icono = "error");
-    const carta = document.createElement("div");
-    carta.classList.add("carta", cartaPinta.toLowerCase());
-    carta.innerHTML =
-      '<span class="carta-numero-pinta top">' +
-      cartaNumero +
-      icono +
-      "</span>" + //por alguna razon los iconos de espada y trebol de fontawesome no cargan
-      '<span class="carta-pinta">' +
-      cartaNumero +
-      "</span>" +
-      '<span class="carta-numero-pinta bot">' +
-      cartaNumero +
-      icono +
-      "</span>";
-    document.body.appendChild(carta);
-  }
-  const cartas = mazoDeCartas();
-  cartaRandom(cartas);
+let randomCard = cards[randomCardIndex()];
 
-  console.log("Hello Rigo from the console!");
-};
+function randomNumberIndex() {
+  return Math.floor(Math.random() * numbers.length);
+}
+let randomNumbers = numbers[randomNumberIndex()];
+
+const topSuit = document.querySelector(".top-suit");
+if (randomCard == "♥") {
+  topSuit.style.color = "red";
+}
+if (randomCard == "♦") {
+  topSuit.style.color = "red";
+}
+topSuit.innerHTML = randomCard;
+
+const BottomSuit = document.querySelector(".bottom-suit");
+if (randomCard == "♥") {
+  BottomSuit.style.color = "red";
+}
+if (randomCard == "♦") {
+  BottomSuit.style.color = "red";
+}
+BottomSuit.innerHTML = randomCard;
+
+let numberSeven = document.querySelector(".number");
+numberSeven.innerHTML = randomNumbers;
